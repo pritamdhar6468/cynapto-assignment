47bbad5a004b0a9727f0417c2a035ff6a6cb6a81
Pritamdhar_official <pritamdhar6568@gmail.com>
2024-06-23 15:59:32 +0530

    made some major UI changes

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -11,6 +11,8 @@ import { BsUnlockFill } from "react-icons/bs";
 import { IoPlayCircleSharp, IoPauseCircleSharp } from "react-icons/io5";
 import { IoPlayForward } from "react-icons/io5";
 import { IoPlayBack } from "react-icons/io5";
+import wave from "../assets/waveform.png";
+
 const Editor = () => {
   const videoRef = useRef(null);
 
@@ -20,7 +22,6 @@ const Editor = () => {
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [pointerPosition, setPointerPosition] = useState(0);
-
 
   const togglePlayPause = () => {
     if (videoRef.current.paused) {
@@ -36,7 +37,6 @@ const Editor = () => {
   const updateTime = () => {
     setCurrentTime(videoRef.current.currentTime);
     updatePointerPosition();
-
   };
 
   // Function to handle video end
@@ -44,17 +44,17 @@ const Editor = () => {
     setIsPlaying(false);
     setCurrentTime(0);
     setPointerPosition(0);
-
   };
 
   // Function to handle seeking forward/backward
   const seek = (time) => {
     videoRef.current.currentTime += time;
     updatePointerPosition();
-
   };
+
   const updatePointerPosition = () => {
-    const currentTimePercentage = (videoRef.current.currentTime / duration) * 100;
+    const currentTimePercentage =
+      (videoRef.current.currentTime / duration) * 100;
     setPointerPosition(currentTimePercentage);
   };
 
@@ -62,19 +62,29 @@ const Editor = () => {
     const timelineRect = timelineRef.current.getBoundingClientRect();
     const clickX = e.clientX - timelineRect.left;
     const newPosition = (clickX / timelineRect.width) * 100;
-  
+
     const newTime = (newPosition / 100) * duration;
     videoRef.current.currentTime = newTime;
     setPointerPosition(newPosition);
   };
 
   const handleInputChange = (e) => {
-  const timeInSeconds = parseFloat(e.target.value);
-  if (!isNaN(timeInSeconds) && timeInSeconds >= 0 && timeInSeconds <= duration) {
-    videoRef.current.currentTime = timeInSeconds;
-    updatePointerPosition();
-  }
-};
+    const timeInSeconds = parseFloat(e.target.value);
+    if (
+      !isNaN(timeInSeconds) &&
+      timeInSeconds >= 0 &&
+      timeInSeconds <= duration
+    ) {
+      videoRef.current.currentTime = timeInSeconds;
+      updatePointerPosition();
+    }
+  };
+
+  const handleSliderChange = (e) => {
+    const newTime = (e.target.value / 100) * duration;
+    videoRef.current.currentTime = newTime;
+    setPointerPosition(e.target.value);
+  };
 
   // Effect to set video duration and listen to time update
   useEffect(() => {
@@ -100,13 +110,6 @@ const Editor = () => {
         <Properties />
       </div>
       <div className="bottom bg-white h-[35vh] ">
-        {/* <HiOutlineZoomIn className="w-8 h-8" />
-        <HiOutlineZoomOut className="w-8 h-8" />
-        <HiOutlineScissors className="w-8 h-8" />
-        <FaCopy className="w-8 h-8" />
-        <FaMousePointer className="w-8 h-8" />
-        <FaEye className="w-8 h-8" />
-        <BsUnlockFill className="w-8 h-8" /> */}
         <div className="flex justify-center">
           <button onClick={() => seek(-10)}>
             <IoPlayBack className="w-16 h-16" />
@@ -122,6 +125,27 @@ const Editor = () => {
           <button onClick={() => seek(10)}>
             <IoPlayForward className="w-16 h-16" />
           </button>
+        </div>
+
+        <div className="flex w-full border border-[#0000005c] shadow-lg">
+          <div className="h-[8rem] flex w-60">
+            <div className="flex justify-center w-full h-full items-center">
+              <span className="text-[1.5rem] font-bold">Track 1</span>
+            </div>{" "}
+            <span className="h-full flex flex-col justify-between p-3"><FaEye className="w-8 h-8"/><BsUnlockFill className="w-8 h-8"/></span>
+          </div>
+          <div className="flex w-full relative justify-center items-center">
+            <input
+              type="range"
+              min="0"
+              max="100"
+              step="1"
+              value={pointerPosition}
+              onChange={handleSliderChange}
+              className="w-3/4 slider2 z-50"
+            />
+            {/* <img className="absolute h-[10rem]" src={wave} alt="" /> */}
+          </div>
         </div>
       </div>
     </div>
